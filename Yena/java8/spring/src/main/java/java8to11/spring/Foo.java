791f7a28518ad710a55bc5753e2e1f642ea06c7c
cathy192 <korea@2047>
2020-08-26 15:06:28 +0900

    java Interface

--- Yena/java8/spring/src/main/java/java8to11/spring/Foo.java
+++ Yena/java8/spring/src/main/java/java8to11/spring/Foo.java
@@ -1,24 +1,11 @@
 package java8to11.spring;
 
+import java.lang.module.FindException;
+import java.util.function.*;
+
 public class Foo {
     public static void main(String[] args) {
-        //익명 내부 클래스 -> 람다 식
-        /*RunSomething runSomething = (num) -> {
-         //   System.out.println("Hello");
-            return num + 10;//입력받은 값이 동일한 경우 결과가 같아야함
-        };
-        System.out.println(runSomething.doIt(1));
+        UnaryOperator<Integer> plus =(i) -> i+10;
 
-    }*/
-
-    RunSomething runSomething=new RunSomething() {
-        final int baseNumber=10;
-        @Override
-        public int doIt(int num) {
-            baseNumber++;
-            return num+baseNumber;
-        }
-    };
-        
     }
 }
