ed289658fc50de70f9388c86cc5769fff8d0f459
cathy192 <korea@2047>
2020-08-30 23:51:14 +0900

    date, time

--- Yena/java8/spring/src/main/java/java8to11/spring/App.java
+++ Yena/java8/spring/src/main/java/java8to11/spring/App.java
@@ -1,6 +1,11 @@
 package java8to11.spring;
 
-import java.time.Duration;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -10,33 +15,27 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App  {
-    public static void main(String[] args) throws IllegalAccessException {
-
-        List<OnlineClass> springClass = new ArrayList<>();
-        springClass.add(new OnlineClass(1,"spring boot", true));
-
-        springClass.add(new OnlineClass(5,"rest api develoment", false));
-
-        Optional<OnlineClass> spring = springClass.stream()
-                .filter(pc -> pc.getTitle().startsWith("spring"))
-                .findFirst();
-        //있으면 꺼내겠다. NULL 체크 안해도 됨
-
-        spring.ifPresent(oc -> System.out.println(oc.getTitle()));
-        //없으면에러 던짐
-        OnlineClass onlineClass1 =  spring.orElseThrow(IllegalAccessException::new);
-        System.out.println(onlineClass1.getTitle());
-        //없으면 새로운 class를 만들겠다.
-        OnlineClass onlineClass =  spring.orElseGet(App::creatNewClass);
-
-        Optional<OnlineClass> onlineClass2 = spring.
-                filter(oc -> oc.isClosed());
-
+    public static void main(String[] args) throws IllegalAccessException, InterruptedException {
+        Date date = new Date();
+        Instant instant = date.toInstant();
+        Date newDAte =Date.from(instant);
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        ZonedDateTime dateTime1 = gregorianCalendar.toInstant()
+                .atZone(ZoneId.systemDefault());//인스턴스만 있으면 다 바꿀수있음
+        //gragorian
+        GregorianCalendar from = GregorianCalendar.from(dateTime1);
+        LocalDateTime dateTime = LocalDateTime.now();
+        dateTime.plus(10, ChronoUnit.DAYS);//기존 인스턴스 변경 안함
+        System.out.println(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        ZoneId zoneId = TimeZone.getTimeZone("PST").toZoneId();//예전에서 최근
+        TimeZone.getTimeZone(zoneId); //최근에서 예전
+        //내가 원하는 데로 포메팅
+        DateTimeFormatter mmddyyy =DateTimeFormatter.ofPattern("MM/dd/yyy");
+        System.out.println(dateTime.format(mmddyyy));
+        //파싱
+        LocalDate parse =LocalDate.parse("07/15/1982",mmddyyy);
+        System.out.println(parse);
     }
 
-    private static OnlineClass creatNewClass() {
-        System.out.println("creat new class");
-        return new OnlineClass(10,"new class",false);
-    }
 
 }
