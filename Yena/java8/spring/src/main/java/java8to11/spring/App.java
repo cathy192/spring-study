fe60a71a70c7467b01592c8bdbc8dc604827d346
cathy192 <korea@2047>
2020-08-31 02:20:14 +0900

    Executor

--- Yena/java8/spring/src/main/java/java8to11/spring/App.java
+++ Yena/java8/spring/src/main/java/java8to11/spring/App.java
@@ -7,6 +7,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -16,24 +17,19 @@ import java.util.stream.Stream;
 
 public class App  {
     public static void main(String[] args) throws IllegalAccessException, InterruptedException {
-        Thread thread =new Thread(() -> {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit( () ->{
+            System.out.println("Tread"+ Thread.currentThread().getName());
+        });//다음작업 들어올떄까지 프로세서가 대기하기에 shutdown으로 꺼줘야함
+        executorService.shutdown();//현재 진행하는 작업을 마치고 shutdown
+       // executorService.shutdownNow()//바로 다 죽임
+        ScheduledExecutorService executorService2 = Executors.newSingleThreadScheduledExecutor();
+        executorService2.schedule(getRunnable("hello"),3, TimeUnit.SECONDS);
 
-                System.out.println("Thread" + Thread.currentThread().getName());
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    throw new IllegalStateException(e);
-                    //  System.out.println("exit");
-                    //return;//void라서 리턴 없음. 종료
-                }
+    }
 
-        });
-        thread.start();
-        System.out.println("Hello"+Thread.currentThread().getName());
-        thread.join();//위에 쓰레드 끝날때 기다려줌
-        System.out.println(thread+"is finished");
-
-
+    private static Runnable getRunnable(String hello) {
+        return () -> System.out.println(hello + Thread.currentThread().getName());
     }
 
 }
